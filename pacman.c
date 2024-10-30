a7cfac608b466b12f29e513587b2a177c0d1dc0c
Lucas Moura <lsam.lucmoura0410@gmail.com>
2024-10-30 10:36:00 -0300

    First Commit

---
+++ pacman.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    char mapa [5] [10];
+    FILE* f;
+    f = fopen("mapa.txt","r");
+    if (f == 0){
+        printf("Erro ao abrir o mapa");
+        exit(1);
+    }
+
+    for (int i = 0)
+    {
+        /* code */
+    }
+    
+}

8a6511545bf727d0545907daa1414fd1c28f81d5
Lucas Moura <lsam.lucmoura0410@gmail.com>
2024-11-15 11:01:57 -0300

    Fantasmas random

--- pacman.c
+++ pacman.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "pacman.h"
 #include "funcoes.h"
 MAPA m;
 POSICAO heroi;
 
+int direcao_fantasma(int xatual, int yatual, int* xdestino, int* ydestino){
+    int opcoes[4][2]={
+        {xatual, yatual + 1},
+        {xatual, yatual -1},
+        {xatual + 1, yatual},
+        {xatual - 1, yatual}
+    };
+    srand(time(0));
+    for(int i = 0; i < 11; i++){
+        int posicao = rand() %4;
 
+        if(pode_andar(&m, opcoes[posicao][0], opcoes[posicao][1]) && ta_vazia(&m, opcoes[posicao][0], opcoes[posicao][1])){
+            *xdestino = opcoes[posicao][0];
+            *ydestino = opcoes[posicao][1];
+            return 1;
+        }
+    }
+    return 0;
+}
 
 
 void fantasma(){
@@ -16,6 +35,11 @@ void fantasma(){
     for (int i = 0; i < m.linhas; i++){
         for(int j = 0; j < m.colunas; j++){
             if(copia.matriz[i][j] == FANTASMA){
+                int xdestino, ydestino;
+                int encontrou = direcao_fantasma(i, j, &xdestino, &ydestino);
+                if(encontrou){
+                    andando_mapa(&m, i, j, xdestino, ydestino);
+                }
                 if(pode_andar(&m, i, j+1) && ta_vazia(&m, i, j+1)){
                     andando_mapa(&m,i, j, i, j + 1);
                     return;
